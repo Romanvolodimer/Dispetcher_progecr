d714ad6795a2ffcc4af5e63d5b7c202155121260
Romanvolodimer <romanvolodimer@gmail.com>
2026-04-30 12:51:38 +0300

    add fixed code

--- server.js
+++ server.js
@@ -215,15 +215,15 @@ function sendConfigAll(ws) {
   console.log("✅ Авторизація 3 успішна");
 
   // сторінка метрики
-  // сторінка метрики (СИСТЕМА 3)
+
   const page4 = await browser.newPage();
 
   // 🔍 ДЕБАГ
   console.log("LOGIN_URL_3:", LOGIN_URL_3);
   console.log("PAGE_3:", PAGE_3);
 
-  const base3 = new URL(LOGIN_URL_3).origin;
-  const url3 = new URL(PAGE_3, base3).href;
+  const base3_fixed = new URL(LOGIN_URL_3).origin;
+  const url3 = new URL(PAGE_3, base3_fixed).href;
 
   console.log("➡️ FINAL URL:", url3);
 
